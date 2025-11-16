af38c223a6fb83664d22454a4b6e3d4ebd419f44
Nicolas Contiero <git@ncontiero.com>
2025-11-16 18:48:57 -0300

    chore: add catch all to urls

--- config/urls.py
+++ config/urls.py
@@ -3,32 +3,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.views import defaults as default_views
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 from .api.main import api
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(settings.ADMIN_URL, admin.site.urls),
     # API base url
     path("api/", api.urls),
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    # Your stuff: custom urls includes go here
+    # Catch all
+    re_path(
+        r"^.*$",
+        TemplateView.as_view(template_name="pages/home.html"),
+        name="home",
+    ),
 ]
 
 
 if settings.DEBUG:
-    # This allows the error pages to be debugged during development.
-    urlpatterns += [
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
-    ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
--- dkmovie/users/admin.py
+++ dkmovie/users/admin.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.utils.translation import gettext_lazy as _
 
-# from .forms import UserAdminChangeForm
-# from .forms import UserAdminCreationForm
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
-    # form = UserAdminChangeForm
-    # add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Personal info"), {"fields": ("name",)}),
