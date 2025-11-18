63c30e53cb4c507de21a68b1487537b14adcd655
Nicolas Contiero <git@ncontiero.com>
2025-11-18 15:20:52 -0300

    chore: remove unnecessary form

--- dkmovie/src/pages/account/my-account/account-tabs/email-card/index.tsx
+++ dkmovie/src/pages/account/my-account/account-tabs/email-card/index.tsx
@@ -140,152 +140,150 @@ export function UserEmailCard() {
   if (!userId) return null;
 
   return (
-    <Card className="mt-10" asChild>
-      <form>
-        <CardContent className="flex flex-col p-4 sm:p-6">
-          <h3 className="text-lg font-bold">Email</h3>
-          <p className="text-muted-foreground mt-2 text-sm">
-            Enter the email addresses you want to use to log in. Your primary
-            email will be used for account-related notifications.
-          </p>
-          <div className="mt-4 flex flex-col gap-2">
-            {isUserEmailsLoading ? (
-              <Skeleton className="flex h-14 items-center gap-2 px-4">
-                <Skeleton className="bg-background h-1/2 w-1/4" />
-                <Skeleton className="bg-background h-1/2 w-1/12" />
-                <Skeleton className="bg-background h-1/2 w-1/12" />
-              </Skeleton>
-            ) : !userEmails ? (
-              <AddEmailDialog userId={userId} />
-            ) : (
-              userEmails.map(({ email, primary, verified }) => (
-                <div
-                  key={email}
-                  className="flex items-center justify-between rounded-lg border px-4 py-2"
-                >
-                  <div className="flex items-center gap-2">
-                    <span className="text-sm">{email}</span>
-                    {verified ? (
-                      <Badge variant="defaultOutline">Verified</Badge>
-                    ) : (
-                      <Badge variant="destructiveOutline">Unverified</Badge>
-                    )}
-                    {primary ? <Badge variant="success">Primary</Badge> : null}
-                  </div>
-                  <div className="flex items-center gap-2">
-                    {!verified ? (
+    <Card className="mt-10">
+      <CardContent className="flex flex-col p-4 sm:p-6">
+        <h3 className="text-lg font-bold">Email</h3>
+        <p className="text-muted-foreground mt-2 text-sm">
+          Enter the email addresses you want to use to log in. Your primary
+          email will be used for account-related notifications.
+        </p>
+        <div className="mt-4 flex flex-col gap-2">
+          {isUserEmailsLoading ? (
+            <Skeleton className="flex h-14 items-center gap-2 px-4">
+              <Skeleton className="bg-background h-1/2 w-1/4" />
+              <Skeleton className="bg-background h-1/2 w-1/12" />
+              <Skeleton className="bg-background h-1/2 w-1/12" />
+            </Skeleton>
+          ) : !userEmails ? (
+            <AddEmailDialog userId={userId} />
+          ) : (
+            userEmails.map(({ email, primary, verified }) => (
+              <div
+                key={email}
+                className="flex items-center justify-between rounded-lg border px-4 py-2"
+              >
+                <div className="flex items-center gap-2">
+                  <span className="text-sm">{email}</span>
+                  {verified ? (
+                    <Badge variant="defaultOutline">Verified</Badge>
+                  ) : (
+                    <Badge variant="destructiveOutline">Unverified</Badge>
+                  )}
+                  {primary ? <Badge variant="success">Primary</Badge> : null}
+                </div>
+                <div className="flex items-center gap-2">
+                  {!verified ? (
+                    <Button
+                      type="button"
+                      variant="outline"
+                      size="sm"
+                      onClick={() => {
+                        resendEmailVerificationMutation(email);
+                      }}
+                      disabled={isResendEmailVerificationPending}
+                    >
+                      {isResendEmailVerificationPending ? (
+                        <Loader className="animate-spin" />
+                      ) : (
+                        "Resend verification email"
+                      )}
+                    </Button>
+                  ) : null}
+                  <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild>
                       <Button
                         type="button"
-                        variant="outline"
-                        size="sm"
-                        onClick={() => {
-                          resendEmailVerificationMutation(email);
-                        }}
-                        disabled={isResendEmailVerificationPending}
+                        variant="ghost"
+                        size="icon"
+                        className="size-8"
                       >
-                        {isResendEmailVerificationPending ? (
+                        <Ellipsis />
+                        <span className="sr-only">Open menu for {email}</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {!primary ? (
+                        <DropdownMenuItem
+                          aria-label="Set as primary email"
+                          className="cursor-pointer"
+                          onClick={() => {
+                            setPrimaryEmailMutation(email);
+                          }}
+                          disabled={isSetPrimaryEmailPending || !verified}
+                        >
+                          Set as primary
+                        </DropdownMenuItem>
+                      ) : null}
+                      <DropdownMenuItem
+                        disabled={primary}
+                        aria-label={
+                          primary
+                            ? "You can't delete the primary email"
+                            : "Delete email"
+                        }
+                        title={
+                          primary
+                            ? "You can't delete the primary email"
+                            : "Delete email"
+                        }
+                        className={`
+                          text-destructive cursor-pointer focus:text-destructive focus:bg-destructive/20
+                          hover:bg-destructive/20
+                        `}
+                        onClick={() => {
+                          setShowDeleteEmailDialog(true);
+                        }}
+                      >
+                        Delete
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+                <AlertDialog
+                  open={showDeleteEmailDialog}
+                  onOpenChange={setShowDeleteEmailDialog}
+                >
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently
+                        remove this email ({email}) from your account.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <Button
+                        type="button"
+                        variant="destructive"
+                        onClick={() => deleteEmailMutation(email)}
+                        disabled={isDeleteEmailPending}
+                      >
+                        {isDeleteEmailPending ? (
                           <Loader className="animate-spin" />
                         ) : (
-                          "Resend verification email"
+                          "Delete"
                         )}
                       </Button>
-                    ) : null}
-                    <DropdownMenu modal={false}>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          className="size-8"
-                        >
-                          <Ellipsis />
-                          <span className="sr-only">Open menu for {email}</span>
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent>
-                        {!primary ? (
-                          <DropdownMenuItem
-                            aria-label="Set as primary email"
-                            className="cursor-pointer"
-                            onClick={() => {
-                              setPrimaryEmailMutation(email);
-                            }}
-                            disabled={isSetPrimaryEmailPending || !verified}
-                          >
-                            Set as primary
-                          </DropdownMenuItem>
-                        ) : null}
-                        <DropdownMenuItem
-                          disabled={primary}
-                          aria-label={
-                            primary
-                              ? "You can't delete the primary email"
-                              : "Delete email"
-                          }
-                          title={
-                            primary
-                              ? "You can't delete the primary email"
-                              : "Delete email"
-                          }
-                          className={`
-                            text-destructive cursor-pointer focus:text-destructive focus:bg-destructive/20
-                            hover:bg-destructive/20
-                          `}
-                          onClick={() => {
-                            setShowDeleteEmailDialog(true);
-                          }}
-                        >
-                          Delete
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                  <AlertDialog
-                    open={showDeleteEmailDialog}
-                    onOpenChange={setShowDeleteEmailDialog}
-                  >
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>
-                          Are you absolutely sure?
-                        </AlertDialogTitle>
-                        <AlertDialogDescription>
-                          This action cannot be undone. This will permanently
-                          remove this email ({email}) from your account.
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <Button
-                          type="button"
-                          variant="destructive"
-                          onClick={() => deleteEmailMutation(email)}
-                          disabled={isDeleteEmailPending}
-                        >
-                          {isDeleteEmailPending ? (
-                            <Loader className="animate-spin" />
-                          ) : (
-                            "Delete"
-                          )}
-                        </Button>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                </div>
-              ))
-            )}
-          </div>
-          {userEmails && userEmails.length < 3 ? (
-            <AddEmailDialog userId={userId} />
-          ) : null}
-        </CardContent>
-        <CardFooter>
-          <CardFooterDescription>
-            Emails must be verified to be able to login with them or be used as
-            primary email. You can add up to 3 emails.
-          </CardFooterDescription>
-        </CardFooter>
-      </form>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+            ))
+          )}
+        </div>
+        {userEmails && userEmails.length < 3 ? (
+          <AddEmailDialog userId={userId} />
+        ) : null}
+      </CardContent>
+      <CardFooter>
+        <CardFooterDescription>
+          Emails must be verified to be able to login with them or be used as
+          primary email. You can add up to 3 emails.
+        </CardFooterDescription>
+      </CardFooter>
     </Card>
   );
 }
