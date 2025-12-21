62b757c882ede6d68426218165484c00f00c004f
Nicolas Contiero <git@ncontiero.com>
2025-12-20 21:43:00 -0300

    fix: add language when retrieving episodes

--- dkmovie/src/http/get-episodes.ts
+++ dkmovie/src/http/get-episodes.ts
@@ -1,8 +1,9 @@
 import type { Episode } from "@/utils/types";
+import { getLangCookie } from "@/utils/cookies";
 import { httpClient } from "./client";
 
 export async function getSeasonEpisodes(titleId: string, seasonNumber: number) {
   return await httpClient.get<Episode[]>(
-    `/titles/${titleId}/season/${seasonNumber}/episodes`,
+    `/titles/${titleId}/season/${seasonNumber}/episodes?lang=${getLangCookie()}`,
   );
 }
