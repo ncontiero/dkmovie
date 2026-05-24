ca81536dfe7de64939a6216b3cf15c20de661ec6
Nicolas Contiero <git@ncontiero.com>
2026-05-23 22:26:49 -0300

    chore(deps): update django to v6

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,8 +45,6 @@ DATABASES = {
     ),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -235,7 +233,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Nicolas Contiero""", "admin@ncontiero.com")]
+ADMINS = ['"Nicolas Contiero" <admin@ncontiero.com>']
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
